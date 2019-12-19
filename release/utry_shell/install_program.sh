4dc06f679c172f5d54b2906643c802db147044aa
jinmenglei <229143434@qq.com>
2019-12-19 14:29:20 +0800

    修复了先前兼容的问题

--- release/utry_shell/install_program.sh
+++ release/utry_shell/install_program.sh
@@ -7,8 +7,8 @@ echo "step 2 chmod start file"
 sudo chmod a+x /etc/profile.d/start_ui.sh
 echo "step 3 cp ubuntu.desktop to computer"
 sudo cp install_ubuntu.desktop /usr/share/xsessions/ubuntu.desktop
-echo "step 4 cp rc.local to computer"
-#sudo cp ./rc.local /etc/
+echo "step 4 cp start_ui to computer"
+sudo cp /home/utry/release/utry_shell/start_ui /usr/bin/
 echo "register cmd"
 RET=$(grep "complete -W" ~/.bashrc)
 CODE=" complete -W 'start stop install uninstall test deltest' start_ui"
